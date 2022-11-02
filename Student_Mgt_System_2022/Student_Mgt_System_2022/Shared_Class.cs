de9744aecdfb8f2707544c528d86f4202b59b161
PoojaKanase24 <112701484+PoojaKanase24@users.noreply.github.com>
2022-11-02 23:03:46 +0530

    Assignment -03

---
+++ Student_Mgt_System_2022/Student_Mgt_System_2022/Shared_Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Student_Mgt_System_2022
+{
+    class Shared_Class
+    {
+        public static string Username = "";
+    }
+}
